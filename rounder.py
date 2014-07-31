bb37f81e28fb126d750af2bb33b7afff9e273694
Bill Frasure <bill.frasure@gmail.com>
2014-07-31 16:03:34 -0600

    Fix PEP8 issues

--- rounder.py
+++ rounder.py
@@ -1,23 +1,24 @@
 # Test data defined in assignment
-# This is stored in unorderd maps, since I don't feel that the order of testing is significant.
+# This is stored in unorderd maps, since I don't feel that the order of testing 
+# is significant.
 values50 = {
     54:50,
     75:100,
     98:100,
     119:100,
-}
+    }
 
 values22 = {
     22:22,
     29:22,
     33:44,
     47:44,
-}
+    }
 
 fullData = {
         50: values50,
         22: values22,
-}
+    }
 
 """ 
 Return number rounded to nearest multiple of base
@@ -30,17 +31,17 @@ startNum -- Number to be rounded
 base -- Base to determine rounding values
 """
 def round_to_base(startNum, base):
-        assert base > 0, "Invalid base for rounding"
-        # Left side of the decimal point is the number of times the base 
-        # can be divided into the number.
-        # Right hand side determines whether you will go up or down for 
-        # nearest multiple.
-        firstDiv = (float(startNum)/base)
+    assert base > 0, "Invalid base for rounding"
+    # Left side of the decimal point is the number of times the base
+    # can be divided into the number.
+    # Right hand side determines whether you will go up or down for
+    # nearest multiple.
+    firstDiv = (float(startNum)/base)
 
-        result = int(base * round(firstDiv))
-        if result == 0:
-            result = base
-        return result
+    result = int(base * round(firstDiv))
+    if result == 0:
+        result = base
+    return result
 
 
 # Iterate through maps of test data with corresponding base
